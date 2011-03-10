7316075cc710f464c02fa253c9caa556a61604ff
Wang Sha <wangshasg@gmail.com>
2011-03-10 13:49:38 -0500

    initial commit

---
+++ buzzbuy_constants.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Tables
+ */
+define('BUZZBUY_PURCHASELIST', 'buzzbuy_purchaselist');
+define('BUZZBUY_WISHLIST','buzzbuy_wishlist');
+define('BUZZBUY_FB_FRIENDLIST',	'buzzbuy_fb_friendlist');
+define('BUZZBUY_FOLLOWING',	'buzzbuy_following');
+define('BUZZBUY_FOLLOWED', 'buzzbuy_followed');
+
+
+
+?>
